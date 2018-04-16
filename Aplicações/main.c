0b3389b4253b1cf11cd60fe94081920c18ba6553
EZMir <eduardomirolli7@gmail.com>
2018-04-16 15:18:45 -0300

    aplicações

---
+++ Aplicações/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]){
+	
+	//PARA ESSE CODIGO FAÇA A IMPRESSAO DE UMA ARVORE NIVEL A NIVEL , USANDO FILA E BUSCA EM LARGURA. Consulte o trabalho de org comp
+
+	return 0;
+}
