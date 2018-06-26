83c95b0522896eb9f3000b6506bf4f75c8beb703
EdZM <eduardomirolli7@gmail.com>
2018-06-26 12:12:02 -0300

    Formatação ajustada/ Comentário inicial adicionado

--- GAME/Guess.cpp
+++ GAME/Guess.cpp
@@ -1,22 +1,15 @@
+/*
+	Arquivo com os métodos da classe Guess, responsável por gerenciar e verificar as tentativas vindas de qualquer jogador
+*/
+
 #include <iterator>
 #include "Guess.h"
 
-Guess::Guess(
-				vector<char>guess,
-				int howManyRight,
-				int howManyInPosition
-			):
-myString(guess),
-score(howManyRight, howManyInPosition)
-{}
+Guess::Guess( vector<char>guess, int howManyRight, int howManyInPosition):
+myString(guess), score(howManyRight, howManyInPosition){}
 
-void Guess::Display() const
-{
-	copy(
-			myString.begin(),
-			myString.end(),
-			ostream_iterator<char>(cout," ")
-		);
+void Guess::Display() const{
+	copy(myString.begin(), myString.end(), ostream_iterator<char>(cout," ")	);
 	cout << "\t";
 	cout << score.first;
 	cout << " corretas, ";
