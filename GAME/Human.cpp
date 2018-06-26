9ec3914772ae9bdafe10c85540ec95d8c671c911
EdZM <eduardomirolli7@gmail.com>
2018-06-26 11:44:50 -0300

    Update Human.cpp
    Formatação ajustada, Comentários incluídos

--- GAME/Human.cpp
+++ GAME/Human.cpp
@@ -1,3 +1,10 @@
+/*
+    Arquivo com os métodos da classe Human, que é responsavel por gerenciar o jogo do humano, contabilizar seus pontos, verificar tentativas 
+    erradas/certas, montar seu historico de tentativas, mostrar dicas, etc.
+    
+*/
+
+
 #include "Human.h"
 #include "Game.h"
 #include <string>
@@ -5,51 +12,45 @@
 
 Human::Human(bool dupes):
 Decrypter(dupes),
-hintCtr(0)
-{
+hintCtr(0){
 
 }
 
-Human::~Human()
-{
+Human::~Human(){
 
 }
 
-vector<char> Human::GetSolution() const
-{
+vector<char> Human::GetSolution() const{
     return solution;
 }
 
-// Handle any flag from user (-?,-q, etc.)
-bool Human::HandleFlag(char flag)
-{
+// manipular caracteres especificos digitados pelo usuario. Esses estao associados com as opções do menu
+bool Human::HandleFlag(char flag){
     bool quit = false;
-    switch (flag)
-    {
-    case 'h':
-        ShowHint();
-        break;
-    case 's':
-        ShowHistory();
-        break;
-    case '?':
-        ShowHelp();
-        break;
-    case '!':
-        Display(GetSolution());
-        break;
-    case 'q':
-        quit = true;
-        break;
-    default:
-        cout << endl << "Comando desconhecido. Ignorado." << endl;
-        break;
+    switch (flag) {
+        case 'h':
+            ShowHint();
+            break;
+        case 's':
+            ShowHistory();
+            break;
+        case '?':
+            ShowHelp();
+            break;
+        case '!':
+            Display(GetSolution());
+            break;
+        case 'q':
+            quit = true;
+            break;
+        default:
+            cout << endl << "Comando desconhecido. Ignorado." << endl;
+            break;
     }
     return quit;
 }
 
-bool Human::IsValid(char theChar) const
-{
+bool Human::IsValid(char theChar) const{
     bool isValid = false;
 
     for ( int i = 0; i < Game::howManyLetters; i++ )
@@ -59,8 +60,7 @@ bool Human::IsValid(char theChar) const
     return isValid;
 }
 
-void Human::Play()
-{
+void Human::Play(){
     vector<char> thisGuess;
     int correct = 0;
     int position = 0;
@@ -69,15 +69,13 @@ void Human::Play()
 
     srand( (unsigned)time( NULL ) );
 
-    for ( int i = 0; i <
-        Game::howManyPositions; )
-    {
+    for ( int i = 0; i < Game::howManyPositions; ) {
+        
         int nextValue = rand() % (Game::howManyLetters);
         char theChar = alpha[nextValue];
-        if ( ! duplicates && i > 0 )
-        {
-            vector<char>::iterator where =
-                find(solution.begin(), solution.end(),theChar);
+        
+        if ( ! duplicates && i > 0 ){
+            vector<char>::iterator where = find(solution.begin(), solution.end(),theChar);
             if ( where != solution.end() )
                 continue;
         }
@@ -85,8 +83,7 @@ void Human::Play()
         i++;
     }
 
-    while ( position < Game::howManyPositions )
-    {
+    while ( position < Game::howManyPositions ){
         thisGuess.clear();
         string guess;
 
@@ -99,16 +96,16 @@ void Human::Play()
 
         cin >> guess;
 
-        if ( guess[0] == '-' )// got a flag
-        {
+        if ( guess[0] == '-' ){
+            
             quit = HandleFlag(guess[1]);
             if ( quit )
                 break;
             continue;
         }
 
-        if ( guess.length() < Game::howManyPositions )
-        {
+        if ( guess.length() < Game::howManyPositions ){
+
             cout << endl << "** Por favor, digite entre exatamente ";
             cout << Game::howManyPositions << " letras. **" << endl;
             continue;
@@ -116,24 +113,18 @@ void Human::Play()
 
         bool lineIsValid = true;
 
-        // ensure they've entered only possible letters
-        for ( int i = 0; i < Game::howManyPositions; i++)
-        {
+        // Garante que o que foi digitado foi apenas letras
+        for ( int i = 0; i < Game::howManyPositions; i++) {
             lineIsValid = IsValid(guess[i]);
             if ( ! lineIsValid )
                 break;
         }
 
-        // create the guess for display
+        // Cria uma tentativa que será exibida
         if ( lineIsValid )
-            for (
-                int i = 0;
-                i < Game::howManyPositions;
-                i++
-                )
+            for ( int i = 0; i < Game::howManyPositions; i++ )
                 thisGuess.push_back(guess[i]);
-        else
-        {
+        else {
 
             cout << "Por favor, digite apenas letras entre ";
             cout << alpha[0] << " e ";
@@ -145,18 +136,18 @@ void Human::Play()
         cout << endl << "Sua tentativa: ";
         Display(thisGuess);
 
-        // compute and report how they did
+        // processando como o jogador se saiu em sua tentativa
         Score(thisGuess,correct,position);
         cout << "\t\t" << correct << " corretas, ";
         cout << position << " em posicao." << endl;
 
-        // crate a record and record it in the history vector
+        
+        // salvando a tentativa em um vetor de tentativas usadas até o momento
         Guess thisRound(thisGuess,correct,position);
         history.push_back(thisRound);
     }
 
-    if ( ! quit )
-    {
+    if ( ! quit ) {
         cout << endl << "A resposta: ";
         Display(GetSolution());
         cout << "\n\n" << endl;
@@ -174,46 +165,33 @@ void Human::Play()
 }
 
 
-// human guessing, see how many letters & position match
-void Human::Score(
-                  vector<char> thisGuess,
-                  int & correct,
-                  int & position
-                  )
-{
+// metodo para verificar como o jogador humano se saiu: quantas letras acertou e quantas estão em posição
+void Human::Score( vector<char> thisGuess, int & correct, int & position ){
     correct = 0;
     position = 0;
     int i;
 
-    // for every possible letter,
-    // how many are in each the answer and the guess
-    for ( i = 0; i < Game::howManyLetters; i++)
-    {
-        int howManyInGuess = count(
-                            thisGuess.begin(),
-                            thisGuess.end(),
-                            alpha[i]
-                            );
-        int howManyInAnswer = count(
-                                    solution.begin(),
-                                    solution.end(),
-                                    alpha[i]
-                                    );
+    // para cada letra possivel
+    // quantas estão em cada posição da resposta e em cada posição da tentativa
+    for ( i = 0; i < Game::howManyLetters; i++) {
+        
+        int howManyInGuess = count( thisGuess.begin(), thisGuess.end(), alpha[i] );
+        int howManyInAnswer = count( solution.begin(), solution.end(),  alpha[i] );
         correct += min(howManyInGuess, howManyInAnswer);
+    
     }
 
-    // for each position in the guess
-    // how many match the solution
-    for (  i = 0; i < Game::howManyPositions; i++)
-    {
+    //para cada posição da tentativa,
+    // quantas posições batem com a resposta
+    for (  i = 0; i < Game::howManyPositions; i++) {
         if ( thisGuess[i] == solution[i] )
             position++;
     }
 }
 
-// When human is guessing, reveal each letter one by one
-void Human::ShowHint()
-{
+
+// metodo para revelar ao jogador letra por letra da resposta, a cada chamada
+void Human::ShowHint(){
     if ( hintCtr < Game::howManyPositions ) {
         cout << "\nDICA!! Posicao ";
         cout << hintCtr+1 << ": ";
@@ -221,9 +199,8 @@ void Human::ShowHint()
         hintCtr++;
     }
 }
-    // result of pressing -?
-void Human::ShowHelp()
-{
+// metodo para processar o resultador de digitar "-?"
+void Human::ShowHelp(){
     cout << "\t-h Dica\n\t-s Mostrar historico\n\t";
     cout << "-? Ajuda\n\t-q Sair\n" << endl;
 }
