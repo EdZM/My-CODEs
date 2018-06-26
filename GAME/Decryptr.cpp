33580e1bb8fec1e1a527f17943109ebc1fa0c302
EdZM <eduardomirolli7@gmail.com>
2018-06-26 12:16:01 -0300

    Comentário inicial adicionado

--- GAME/Decryptr.cpp
+++ GAME/Decryptr.cpp
@@ -1,3 +1,9 @@
+/*
+    Arquivo com os métodos da classe decryptr, responsável por manusear a tentativa feita por qualquer jogador, seja ele humano ou
+    computador.
+
+*/
+
 #include "Decryptr.h"
 
 Decrypter::Decrypter(bool hasDuplicates):
@@ -15,7 +21,7 @@ void Decrypter::Display(vector<char> charVec) const {
     copy(charVec.begin(), charVec.end(), ostream_iterator<char>(cout," "));
 }
 
-// Iterate through history of guesses and display results
+// método para iterar no historico de tentativas e mostrar os resultados
 void Decrypter::ShowHistory() {
     for(vector<Guess>::const_iterator it = history.begin(); it != history.end(); it++) {
         it->Display();
