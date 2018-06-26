4b4793cff9e712a6af99e3c7150f4e6b0610fc06
EdZM <eduardomirolli7@gmail.com>
2018-06-26 11:50:37 -0300

    Formatação concluída/comentário inicial adicionado

--- GAME/SmartChr.cpp
+++ GAME/SmartChr.cpp
@@ -1,39 +1,37 @@
+/*
+    Arquivo com os métodos da classe SmartChar, responsável por verificar a válidade da tentativa gerada pelo computador quando ele estiver 
+    jogando, caractere a caractere
+
+*/
 
 #include "SmartChar.h"
 #include "Game.h"
 
 SmartChar::SmartChar(int letter):
-myChar(letter)
-{
+myChar(letter){
     for ( int i = 0; i < Game::howManyLetters; i++ )
         myCharacters.push_back(alpha[i]);
 }
 
-SmartChar::~SmartChar()
-{
+SmartChar::~SmartChar(){
 }
 
-char SmartChar::GetChar() const
-{
+char SmartChar::GetChar() const{
     return myCharacters[myChar];
 }
 
-bool SmartChar::Increment()
-{
-    if ( ++myChar >= myCharacters.size() )
-    {
+bool SmartChar::Increment(){
+    if ( ++myChar >= myCharacters.size() )  {
         myChar = 0;
         return true;
     }
     return false;
 }
 
-char SmartChar::RemoveCurrent()
-{
+char SmartChar::RemoveCurrent(){
     char theChar = ' ';
 
-    if ( myCharacters.size() > 1 )
-    {
+    if ( myCharacters.size() > 1 ){
         theChar = GetChar();
         myCharacters.erase( myCharacters.begin()+myChar );
         while ( myChar >= myCharacters.size() )
@@ -42,12 +40,9 @@ char SmartChar::RemoveCurrent()
     return theChar;
 }
 
-bool SmartChar::Remove(char theChar)
-{
-    if ( myCharacters.size() > 1 )
-    {
-        vector<char>::iterator where =
-            find(myCharacters.begin(), myCharacters.end(),theChar);
+bool SmartChar::Remove(char theChar){
+    if ( myCharacters.size() > 1 ){
+        vector<char>::iterator where = find(myCharacters.begin(), myCharacters.end(),theChar);
         if ( where != myCharacters.end() )
             myCharacters.erase(where);
         return true;
