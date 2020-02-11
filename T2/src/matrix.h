12c0eda62f57cd742c480462433572ccf803135c
Eduardo Mirolli <eduardomirolli7@gmail.com>
2020-02-11 06:51:02 +0000

    novas configurações

--- T2/src/matrix.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATRIX_H
-#define MATRIX_H
-
-void matrix_create(int*** matrix, int row, int col);
-void generate_matrix(int** matrix, int row, int col, int seed);
-void print_matrix(int** matrix, int row, int col);
-
-#endif
